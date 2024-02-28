7a158387be9eb53dd0e94dafa31f491e7e4d5a84
vaishnaviR634 <120177813+vaishnaviR634@users.noreply.github.com>
2024-02-29 02:05:04 +0530

    Update exchange.cpp

--- exchange.cpp
+++ exchange.cpp
@@ -16,11 +16,7 @@ struct order {
 	int64_t quantity;
     int64_t time;
 	bool buy; //0=buy,1=sell
-	bool matched;
-    // order(int64_t ID, long double price, int64_t quantity,bool buy  ){
-    //     this->buy=buy; this->price=price;this->quantity=quantity;
-    //     this->matched=0; this->time=//add time;
-    // }
+	//bool matched;
     order();
        bool operator<( order &other) {
         if (price != other.price) {
@@ -36,7 +32,7 @@ order parse(std::shared_ptr<char[]> buffer) {
 	std::istringstream stream(buffer.get());
 	order ord;
 	stream >> ord.order_id >> ord.buy >> ord.price >> ord.quantity;
-	ord.matched = false;
+	//ord.matched = false;
      ord.time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 	return ord;
 }
@@ -45,16 +41,14 @@ int64_t ask_price,ask_quantity,bid_price,bid_quantity;
 //ask price-> highest seller acceptence price
 std::priority_queue<order> BUY,SELL;
 
-void add_to_priority_queue(order ord) {
+void add_to_priority_queue(const order& ord) {
 	 //take the order, add it to the necessary priority queue
-     bool display=false;
        if (ord.buy) {
               BUY.push(ord);
             if (ord.price >= ask_price) {
                 match();
                 return;
             }
-          //  if (ord.price >= bid_price) display = true;
 
         } else {
               SELL.push(ord);
@@ -62,46 +56,34 @@ void add_to_priority_queue(order ord) {
                 match();
                 return;
             }
-           // if (ord.price <= ask_price) display = true;
         }
 
 }
+order& buy_top = const_cast<order&>(BUY.top());
+        order& sell_top = const_cast<order&>(SELL.top());
 
 void match() {
 	// match the orders
 
 while(BUY.size()>0 && SELL.size()>0){
 
- order& buy_top = const_cast<order&>(BUY.top());
-        order& sell_top = const_cast<order&>(SELL.top());
+ buy_top = const_cast<order&>(BUY.top());
+  sell_top = const_cast<order&>(SELL.top());
 
           if(buy_top.price < sell_top.price){break;}
 
         if(buy_top.quantity > sell_top.quantity){
-          
-             std::cout<<"Order No: "<<buy_top.order_id<<std::endl;
-             std::cout<<"Partial Buy Order executed! Quantity Bought: "<<sell_top.quantity;
-                std::cout<<"Order No: "<<sell_top.order_id<<std::endl;
-             std::cout<<"Sell Order executed!";
-
+              display(0,sell_top.quantity);
                 SELL.pop();
             buy_top.quantity-=sell_top.quantity;
         }
         else if(buy_top.quantity < sell_top.quantity){
-            
-             std::cout<<"Order No: "<<sell_top.order_id<<std::endl;
-             std::cout<<"Partial Sell Order executed! Quantity Sold: "<<buy_top.quantity;
-                std::cout<<"Order No: "<<buy_top.order_id<<std::endl;
-             std::cout<<"Buy Order executed!";
+                display(1,buy_top.quantity);
             BUY.pop();
             sell_top.quantity-=buy_top.quantity;
         }
         else {  
-            
-             std::cout<<"Order No: "<<buy_top.order_id<<std::endl;
-             std::cout<<" Buy Order executed!";
-                std::cout<<"Order No: "<<sell_top.order_id<<std::endl;
-             std::cout<<"Sell Order executed!";
+            display(2,0);
             BUY.pop(); SELL.pop();
 
         }
@@ -109,5 +91,20 @@ while(BUY.size()>0 && SELL.size()>0){
 
 }
 
+}
 
+void display(int x, long long q){
+      std::cout<<"Order ID: "<<buy_top.order_id<<std::endl;
+              if(x==0)std::cout<<"Partial ";
+             std::cout<<" Buy Order executed!\n"; 
+             long long q1=buy_top.quantity;
+             if(x==0) q1=q;
+             std::cout<<" Quantity Bought: "<<q<<" at Price:"<<sell_top.price;
+
+              std::cout<<"Order ID: "<<sell_top.order_id<<std::endl;
+                  if(x==1)std::cout<<"Partial ";
+             std::cout<<"Sell Order executed!\n";
+             long long q2= sell_top.quantity;
+             if(x==1)q2=q;
+             std::cout<<" Quantity Sold: "<<q<<" at Price:"<<sell_top.price;
 }
